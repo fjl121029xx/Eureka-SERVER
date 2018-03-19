5912dabb15143caf3fbfd21bf9aea72ac9129db5
fjl121029xx <sishengqikuo_xx@163.com>
2018-03-19 08:05:53 +0800

    spring cloud 微服务实战 Eureka 服务提供者

---
+++ src/main/java/com/li/springcloudserverprovider/SpringcloudServerProviderApplication.java
@@ -0,0 +1,17 @@
+package com.li.springcloudserverprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+/**
+ * spring cloud server provider
+ */
+public class SpringcloudServerProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudServerProviderApplication.class, args);
+	}
+}
